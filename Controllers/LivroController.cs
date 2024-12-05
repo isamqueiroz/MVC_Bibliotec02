1b9a51a44b6f01cf7b4647e0f09bdb77406bddd3
isamqueiroz <isa22queiroz@gmail.com>
2024-12-05 16:50:22 -0300

    atualizando...

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Bibliotec.Contexts;
 using Bibliotec.Models;
@@ -64,27 +65,32 @@ namespace Bibliotec_mvc.Controllers
             novoLivro.Escritor = form["Escritor"].ToString();
             novoLivro.Idioma = form["Idioma"].ToString();
 
-            if(form.Files.Count > 0){
+            if (form.Files.Count > 0)
+            {
                 //Primeiro passo
                 //ARmazenaresmos o arquivo/foto enviado pelo meu usuario.
                 var arquivo = form.Files[0];
-                 //Segundo passo
+                //Segundo passo
                 //criar variavel do caminho da minha pasta para colocar as fotos do livro
                 var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Livros");
                 //Validaresmos se a pasta que sera armazenada as imagens, existe. Caso nao exista, criaremos uma nova pasta.
-                if(Directory.Exists(pasta)){
+                if (Directory.Exists(pasta))
+                {
                     //Criar pasta:
                     Directory.CreateDirectory(pasta);
 
-                //terceiro passo:
-                //criar  a variavel para armazenar o caminho  em que meu arquivo estara, alem do nome dele.
-                var caminho = Path.Combine(pasta, arquivo.FileName);
-                
-                using (var stream = new FileStream(caminho, FileMode.Create)){
-                    //Copiou o arquivo para o meu diretorio
-                    arquivo.CopyTo(stream);
+                    //terceiro passo:
+                    //criar  a variavel para armazenar o caminho  em que meu arquivo estara, alem do nome dele.
+                    var caminho = Path.Combine(pasta, arquivo.FileName);
+
+                    using (var stream = new FileStream(caminho, FileMode.Create))
+                    {
+                        //Copiou o arquivo para o meu diretorio
+                        arquivo.CopyTo(stream);
+                    }
                 }
-                }else {
+                else
+                {
                     novoLivro.Imagem = "padrao.png";
                 }
 
@@ -119,5 +125,136 @@ namespace Bibliotec_mvc.Controllers
 
             return LocalRedirect("/Livro/Cadastro");
         }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+
+            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+
+            ViewBag.CategoriasDoSistema = context.Categoria.ToList();
+
+
+            //Diario de um banana 
+            //LivroID == 3
+
+            //Buscar quem e o tal do Id numero 3
+            Livro livroAtualizado = context.Livro.FirstOrDefault(Livro => Livro.LivroID == id)!;
+
+            //Buscar as categorias que o livroAtualizado possui
+            var categoriasDoLivroAtualizado = context.LivroCategoria.Where(identificadorLivro => identificadorLivro.LivroID == id).Select(Livro => Livro.Categoria).ToList();
+
+            //Quero pegar as informacoes e mandar para a minha view 
+            ViewBag.Livro = livroAtualizado;
+            ViewBag.Categoria = categoriasDoLivroAtualizado;
+
+
+            return View();
+        }
+
+        //metodo que atualiza as informacoes do livro 
+
+        [Route("Atualizar/{id}")]
+        public IActionResult Atualizar (IFormCollection form, int id, IFormFile imagem)
+        {
+            //BUScar um Livro especifico pelo ID
+            Livro livroAtualizado = context.Livro.FirstOrDefault(livro => livro.LivroID == id)!;
+
+            livroAtualizado.Nome = form["Nome"].ToString();
+            livroAtualizado.Escritor = form["Escritor"].ToString();
+            livroAtualizado.Editora = form["Editora"].ToString();
+            livroAtualizado.Idioma = form["Idioma"].ToString();
+            livroAtualizado.Descricao = form["Descricao"].ToString();
+
+            if (imagem != null && imagem.Length > 0)
+            {
+                //Definir o caminho da minha imagem
+                var caminhoImagem = Path.Combine("wwwroot/images/Livros", imagem.FileName);
+
+                //verificar se minha imagem ainda existe no meu caminho.
+                if (string.IsNullOrEmpty(livroAtualizado.Imagem))
+                {
+                    //caso exista ela ira ser excluida
+                    var caminhoImagemAntiga = Path.Combine("wwwroot/images/Livros", livroAtualizado.Imagem);
+                    //ver se existe uma imagem no caminho antigo.
+                    if (System.IO.File.Exists(caminhoImagemAntiga))
+                    {
+                        System.IO.File.Delete(caminhoImagemAntiga);
+                    }
+
+                }
+
+                using (var stream = new FileStream(caminhoImagem, FileMode.Create))
+                {
+                    imagem.CopyTo(stream);
+                }
+
+                //subir essa mudanca para o meu banco de dados
+                livroAtualizado.Imagem = imagem.FileName;
+            }
+
+            //CATEGORIAS:
+
+            //PRIMEIRO: Precisamos pegar as categorias selecionadas no usuario
+            var categoriasSelecionadas = form["Categoria"].ToList();
+            //SEGUNDO: Pegaremos as categorias ATUAIS do Livro
+            var categoriasAtuais = context.LivroCategoria.Where(livro => livro.LivroID == id);
+            //TERCEIRO: Removeremos as categorias antigas 
+            foreach (var categoria in categoriasAtuais)
+            {
+                if (!categoriasSelecionadas.Contains(categoria.CategoriaID.ToString()))
+                {
+                    //nos vamos remover a categoria do nosso context
+                    context.LivroCategoria.Remove(categoria);
+                }
+            }
+            //QUARTO: Adicionaremos as novas categorias 
+            foreach (var categoria in categoriasSelecionadas)
+            {
+                if (!categoriasAtuais.Any(c => c.CategoriaID.ToString() == categoria))
+                {
+                    context.LivroCategoria.Add(new LivroCategoria
+                    {
+                        LivroID = id,
+                        CategoriaID = int.Parse(categoria)
+                    });
+                }
+
+                context.SaveChanges();
+
+            }
+                return LocalRedirect("/Livro");
+        }
+
+       [Route("Excluir/{id}")]
+       public IActionResult Excluir(int id){
+        //Buscar qual o livro do id que precisamos excluir
+        //Livro livroEncontrado = context.Livro.First(livro => livro.LivroID == id); 
+        Livro livroEncontrado = context.Livro.First(livro => livro.LivroID == id);
+        //BUscar  as categorias desse livro:
+        var categoriasDoLivro = context.LivroCategoria.Where(livro => livro.LivroID == id).ToList();
+
+        //precisamos excluir primeiro o registro da tabala intermediaria
+        foreach (var categoria in categoriasDoLivro){
+            context.LivroCategoria.Remove(categoria);
+        }
+
+        context.Livro.Remove(livroEncontrado);
+
+        context.SaveChanges();
+        
+
+
+        return LocalRedirect("/Livro");
+       }
+
+
+
+
+
+
     }
+
 }
+
+
--- obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1dff2b900332744a9ccae5bee1b03b344f8b690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b0f178a52481241f839fab647229f4dbeff87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
